104bb16f6278f0ac6b12cefe5944cf607514b4d4
AbdulRehmanRYK <wisemen620@gmail.com>
2022-07-22 16:35:56 +0500

    set different background on hover and add contact form

---
+++ src/pages/contactus.jsx
@@ -0,0 +1,13 @@
+// import LayoutWrapper from "components/layout/LayoutWrapper";
+import ContactUspage from "components/pages/contactUs";
+import React from "react";
+
+const Contact =()=>{
+
+    return(
+        <>
+           <ContactUspage />
+        </>
+    )
+}
+export default Contact;
